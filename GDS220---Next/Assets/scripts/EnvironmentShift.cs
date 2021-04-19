7fd0df0213041ec60d7574d1ac05e4d957c2d6e0
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2021-04-19 14:41:40 +0800

    mechanics scripts added

---
+++ GDS220---Next/Assets/scripts/EnvironmentShift.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentShift : MonoBehaviour
+{
+    Renderer renderer;
+
+    public GameObject otherModel, currentModel;
+    int visCounter;
+    bool previousFrame, currentFrame;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (renderer.isVisible)
+        {
+            Debug.Log("Object is visible");
+            currentFrame = true;
+        }
+        else
+        {
+            Debug.Log("Object is no longer visible");
+            currentFrame = false;
+            OnBecameInvisible();
+            
+        }
+
+        previousFrame = currentFrame;
+    }
+
+    void OnBecameInvisible()
+    {
+        if (currentFrame == false && previousFrame == true)
+        {
+            currentModel.SetActive(false);
+            otherModel.SetActive(true);
+        }
+        //enabled = false;
+    }
+}
---
+++ GDS220---Next/Assets/scripts/ObjectController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectController : MonoBehaviour
+{
+    [SerializeField] private string objectName;
+    [TextArea] [SerializeField] private string objectExtraInfo;
+
+    [SerializeField] private InspectController inspectController;
+
+    public void ShowObjectName()
+    {
+        inspectController.ShowName(objectName);
+    }
+
+    public void HideObjectName()
+    {
+        inspectController.HideName();
+    }
+
+    public void ShowExtraInfo()
+    {
+        inspectController.ShowAdditionalInfo(objectExtraInfo);
+    }
+}
