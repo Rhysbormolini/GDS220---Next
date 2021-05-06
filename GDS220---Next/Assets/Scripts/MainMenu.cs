16afa5de6dfc175080de5bbb904bf89935600f86
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2021-05-06 13:38:13 +0800

    Menu Code, TMP, Music

---
+++ GDS220---Next/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject title, options, credits;
+
+    // Play the game 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // open the options menu
+    public void OptionsMenu()
+    {
+        title.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        title.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        title.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void CreditsMenu()
+    {
+        title.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
